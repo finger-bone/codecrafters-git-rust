b23fa65aeb3a3036bf9e1ec168f3f3d87a4a5686
zend <zendwang040302@outlook.com>
2024-05-18 20:08:29 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use flate2::read::ZlibDecoder;
+
+fn cat_file_handler(args: &Vec<String>) {
+    let blob_sha = &args[3];
+    let blob_path = format!(".git/objects/{}/{}", &blob_sha[0..2], &blob_sha[2..]);
+
+    let mut file = File::open(Path::new(&blob_path)).expect("Unable to open file");
+    let mut compressed_content = Vec::new();
+    file.read_to_end(&mut compressed_content).expect("Unable to read file");
+
+    let mut decoder = ZlibDecoder::new(&compressed_content[..]);
+    let mut decompressed_content = Vec::new();
+    decoder.read_to_end(&mut decompressed_content).expect("Unable to decompress");
+
+    let null_index = decompressed_content.iter().position(|&b| b == b'\0').expect("Null byte not found");
+    let content = &decompressed_content[(null_index + 1)..];
+
+    print!("{}", String::from_utf8_lossy(content));
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,6 +35,8 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        cat_file_handler(&args);
     } else {
         println!("unknown command: {}", args[1])
     }
