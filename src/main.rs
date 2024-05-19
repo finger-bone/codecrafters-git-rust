1fa556a24dff7aa0812b9d86f155d2e0a8f57793
zend <zendwang040302@outlook.com>
2024-05-19 10:14:28 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ fn hash_object_handler(args: &Vec<String>) {
     let sha1_hash_str = format!("{:x}", sha1_hash);
 
     let blob_path = format!(".git/objects/{}/{}", &sha1_hash_str[0..2], &sha1_hash_str[2..]);
+    fs::create_dir_all(Path::new(&blob_path).parent().unwrap()).expect("Unable to create directory");
     let mut encoder = ZlibEncoder::new(File::create(blob_path).expect("Unable to create file"), Compression::default());
     encoder.write_all(blob_content.as_bytes()).expect("Unable to write to file");
     encoder.write_all(&content).expect("Unable to write to file");
